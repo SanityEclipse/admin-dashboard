97a190c200231d8178a3dd1f58266dfb8a07e156
Thomas N Rizzo III <thomasnrizzo@gmail.com>
2017-07-10 11:48:45 -0600

    Added correct route to router for redirect.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-export const routes: Routes = [{}]
+export const routes: Routes = [
+  { path: '', redirectTo: '/', pathMatch: 'full'}
+]
 
 @NgModule({
   declarations: [],
